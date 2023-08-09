8f243659f2dda17ac1a53fb26ea0ca1c35b90125
MehranJavid <mehran.javid12@gmail.com>
2023-08-09 23:00:32 +0500

    conditionally render navigation bar

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,30 +1,42 @@
 import { withLoggedIn } from "./common";
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate, Outlet } from 'react-router-dom';
 import { Login, SignUp, Dashboard, Pricing } from "./pages";
 import { ROUTE_CONSTANTS } from "./common";
+import { NavBar } from "./components";
 
 function App() {
 
   const AuthDashboard = withLoggedIn(Dashboard);
-  const AuthPaymentPlan = withLoggedIn(Pricing);
-  // TODO authenticate on signup only | not on login
+  const AuthPaymentPlan = withLoggedIn(Pricing); // TODO authenticate on signup only | not on login
 
   return (
     // TODO: add suspense with loading fallback to handle loading time delays.
     <BrowserRouter>
       <Routes>
-        {/* Global Routes */}
+
+        <Route path='/' element={<NavBarLayout />}>
+          <Route path={ROUTE_CONSTANTS.SIGN_UP} element={<SignUp />} />
+          <Route path={ROUTE_CONSTANTS.PRICING} element={<AuthPaymentPlan />} />
+          <Route path="*" element={<Navigate to={ROUTE_CONSTANTS.HOME} replace />} />
+        </Route>
+
         <Route path={ROUTE_CONSTANTS.LOGIN} element={<Login />} />
-        <Route path={ROUTE_CONSTANTS.SIGN_UP} element={<SignUp />} />
-
-        {/* Protected Routes */}
         <Route path={ROUTE_CONSTANTS.DASHBOARD} element={<AuthDashboard />} />
-        <Route path={ROUTE_CONSTANTS.PRICING} element={<AuthPaymentPlan />} />
 
-        {/* Default Route */}
-        <Route path="*" element={<Navigate to={ROUTE_CONSTANTS.HOME} replace />} />
       </Routes>
     </BrowserRouter>
+  )
+}
+
+function NavBarLayout () {
+
+  return (
+    <div>
+      <NavBar />
+      <div className="md:container md:mx-auto">
+        <Outlet />
+      </div>
+    </div>
   )
 }
 
