845132563063cd71d828e6488e1314be84d3c87e
MehranJavid <mehran.javid12@gmail.com>
2023-08-29 22:58:47 +0500

    SHD-34 update file name UserReducers.ts to  UserSlice.ts

--- frontend-web/src/redux/index.ts
+++ frontend-web/src/redux/index.ts
@@ -1,7 +1,7 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit"
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
-import user from "./reducers/UserReducers"
+import user from "./reducers/UserSlice"
 import sessionStorage from "redux-persist/es/storage/session"
 
 const rootPersistConfig = {
