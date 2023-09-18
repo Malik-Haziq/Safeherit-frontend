043d1dec1dbc83139a8c03d5aecf8c9eb514d619
Mehran <mehran.javid12@gmail.com>
2023-09-19 00:34:57 +0500

    dashborad drawer selection fix & nification key fix

--- frontend-web/src/pages/dashboard/DashboardNavbar.tsx
+++ frontend-web/src/pages/dashboard/DashboardNavbar.tsx
@@ -3,51 +3,27 @@ import userImg from "../../../assets/images/user.svg"
 import arrowDown from "../../../assets/images/chevron-down.svg"
 import { DropDownButton } from "../../components"
 import { Notifications } from "../../../src/components/notificationDropDown"
-import { useLocation } from "react-router-dom"
 
 type NavBarItem = {
   screen: string
   title: string
 }
 
-export default function DashboardNavbar(_props: { _handleLogout: Function }) {
-  const currentPath = useLocation()
+export default function DashboardNavbar(_props: {
+  _handleLogout: Function
+  navBarHeadings: Record<string, NavBarItem>
+  currentPath: any
+}) {
   const USER_NAME = window.localStorage.getItem("userName") || "Profile"
-  const navBarHeadings: Record<string, NavBarItem> = {
-    "/dashboard": {
-      screen: "Dashboard",
-      title: "View all your assets status",
-    },
-    "/dashboard/assets": {
-      screen: "My Assets",
-      title: "Add and remove assets",
-    },
-    "/dashboard/beneficiaries": {
-      screen: "Beneficiaries",
-      title: "Add and remove beneficiaries",
-    },
-    "/dashboard/validators": {
-      screen: "Validators",
-      title: "Manage your validators",
-    },
-    "/dashboard/pulse": {
-      screen: "Pulse check",
-      title: "Check your pulse",
-    },
-    "/dashboard/account": {
-      screen: "My Account",
-      title: "Manage you information",
-    },
-  }
 
   return (
     <div className="h-[83px] p-2 sm:p-7 flex justify-between items-center shadow-sm min-w-[1200px] max-w-[100vw]">
       <div>
         <h2 className="text-safe-text-black-tint sm:text-xl font-bold">
-          {navBarHeadings[currentPath.pathname].screen}
+          {_props.navBarHeadings[_props.currentPath.pathname].screen}
         </h2>
         <p className="text-safe-text-dark-gray text-sm sm:text-base ">
-          {navBarHeadings[currentPath.pathname].title}
+          {_props.navBarHeadings[_props.currentPath.pathname].title}
         </p>
       </div>
       <div className="flex items-center gap-3 md:gap-10">
