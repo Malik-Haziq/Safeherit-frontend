cbb19cb53483087103170f41afedae1f881113dd
Malik Haziq <malikhaziq153@gmail.com>
2023-10-13 02:06:09 +0500

    screen to setup pulse check

--- frontend-web/src/pages/dashboard/views/pulse/PulseView.tsx
+++ frontend-web/src/pages/dashboard/views/pulse/PulseView.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, MouseEvent } from "react"
 import styles from "../../Dashboard.module.css"
+import pulseImg from "@images/pulse-check-img.svg"
 import {
   StepOneModal,
   StepTwoModal,
@@ -11,7 +12,7 @@ import {
 export default function PulseView() {
   return (
     <div className={styles.AppView}>
-      <StepOneModal
+      {/* <StepOneModal
         openModal={true}
         closeModal={false}
         closeModalOnOverlayClick={false}
@@ -21,8 +22,32 @@ export default function PulseView() {
         _handleChange={function (event: ChangeEvent<HTMLInputElement>): void {
           throw new Error("Function not implemented.")
         }}
-      />
-      <p>Pulse View</p>
+      /> */}
+      <SetUpPulseCheck openStepZeroModal={() => {}} />
+    </div>
+  )
+}
+
+function SetUpPulseCheck(_props: {
+  openStepZeroModal: React.MouseEventHandler<HTMLButtonElement>
+}) {
+  return (
+    <div className="h-[calc(100vh-83px)] p-7">
+      <main className="flex flex-col items-center justify-center shadow-xl h-full rounded-2xl">
+        <img src={pulseImg} className="mb-10 " alt="validator screen image" />
+        <h2 className="text-[#00192B] text-xl font-bold mb-2">
+          No Pulse checked
+        </h2>
+        <p className="text-[#868686] mb-10">
+          You still didn’t set up ho we should make sure you are alive.
+        </p>
+        <button
+          onClick={_props.openStepZeroModal}
+          className="primary-btn rounded-2xl py-3 px-9 bg-[#0971AA]"
+        >
+          Set Up Pulse
+        </button>
+      </main>
     </div>
   )
 }
