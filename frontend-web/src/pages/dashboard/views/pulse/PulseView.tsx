bafc0798997b12c90706a59c072300fea0d57179
Malik Haziq <malikhaziq153@gmail.com>
2023-10-13 01:56:52 +0500

    pulse check modals completed

--- frontend-web/src/pages/dashboard/views/pulse/PulseView.tsx
+++ frontend-web/src/pages/dashboard/views/pulse/PulseView.tsx
@@ -1,8 +1,27 @@
+import { ChangeEvent, MouseEvent } from "react"
 import styles from "../../Dashboard.module.css"
+import {
+  StepOneModal,
+  StepTwoModal,
+  StepThreeModal,
+  StepFourModal,
+  SuccessModal,
+} from "./modal_pulse"
 
 export default function PulseView() {
   return (
     <div className={styles.AppView}>
+      <StepOneModal
+        openModal={true}
+        closeModal={false}
+        closeModalOnOverlayClick={false}
+        closeIconVisibility={true}
+        action={""}
+        _submitModal={() => {}}
+        _handleChange={function (event: ChangeEvent<HTMLInputElement>): void {
+          throw new Error("Function not implemented.")
+        }}
+      />
       <p>Pulse View</p>
     </div>
   )
