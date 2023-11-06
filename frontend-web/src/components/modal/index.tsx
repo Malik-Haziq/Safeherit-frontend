4a215c7cce61a780cbdbcda2af3ff2e16c6e490d
Mehran <mehran.javid12@gmail.com>
2023-11-06 20:31:13 +0500

    Merge branch 'development' of https://bitbucket.org/safe-herit/safeherit_frontend into feature/encryption

--- frontend-web/src/components/modal/index.tsx
+++ frontend-web/src/components/modal/index.tsx
@@ -175,57 +175,22 @@ export const Modal = (_props: {
   const elements = _props?.elements
   return (
     <>
-      <Transition appear show={_props.openModal} as={Fragment}>
-        <Dialog
-          as="div"
-          className="relative z-10"
-          onClose={
-            _props.closeModalOnOverlayClick ? _props.closeModal : () => {}
-          }
-        >
-          <Transition.Child // modal overlay opacity
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 overflow-y-auto ">
-            <div className="flex min-h-full items-center justify-center ">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel className="transform overflow-hidden rounded-2xl bg-white text-center align-middle shadow-xl transition-all">
-                  <div className={styles.modalContainer}>
-                    <div className={styles.modal}>
-                      <ModalHeader
-                        closeModal={_props.closeModal}
-                        title={_props.modalTitle}
-                        closeIconVisibility={_props.closeIconVisibility}
-                        arrayLength={_props.arrayLength}
-                        showPreviousModal={_props.showPreviousModal}
-                      />
-                      <RenderModal elements={elements} />
-                    </div>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
-        </Dialog>
-      </Transition>
-    </>
+      {_props.openModal && (
+        <div
+          className={styles.backDrop}
+        ><div className={styles.modalContainer}>
+        <div className={styles.modal}>
+          <ModalHeader
+            closeModal={_props.closeModal}
+            title={_props.modalTitle}
+            closeIconVisibility={_props.closeIconVisibility}
+          />
+          <RenderModal elements={elements} />
+        </div>
+      </div> </div>
+      )
+}
+  </>
   )
 }
 
