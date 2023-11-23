e2d4e228421799223909f2344ed213a91c6b787a
Mehran <mehran.javid12@gmail.com>
2023-11-24 01:24:22 +0500

    code component remove from 2faAuth

--- frontend-web/src/pages/dashboard/views/account/TwoFAAuth.tsx
+++ frontend-web/src/pages/dashboard/views/account/TwoFAAuth.tsx
@@ -122,6 +122,15 @@ function CodeSignup({currentUser, verificationCodeId, _cancelConfirmationCode, r
   returnToAccountView: Function
 }) {
 
+  let code = new Array<string>(6).fill('');
+
+  function handleClick() {
+    const finalCode = code.reduce((previousValue, currentValue) => {
+      return previousValue.concat(currentValue);
+    })
+    getCode(finalCode);
+  }
+  
   async function getCode(code: string) {
     const response = await enrollUser(
       currentUser,
@@ -139,63 +148,44 @@ function CodeSignup({currentUser, verificationCodeId, _cancelConfirmationCode, r
   }
 
   return (
-    <Code 
-      getCode={getCode}
-      _cancelConfirmationCode={_cancelConfirmationCode}
-    />
-  )
-}
-
-function Code({getCode, _cancelConfirmationCode}: {
-  getCode: (code: string) => void
-  _cancelConfirmationCode: Function
-}) {
-  let code = new Array<string>(6).fill('');
-
-  function handleClick() {
-    const finalCode = code.reduce((previousValue, currentValue) => {
-      return previousValue.concat(currentValue);
-    })
-    getCode(finalCode);
-  }
-
-  return (
-    <div className="bg-white flex flex-col p-5 md:p-6  border-2 border-palladium rounded-xl w-full sm:max-w-[440px]">
-      <div className="flex justify-between">
-        <div>
-          <h1 className='font-medium text-[22px] leading-[130%] md:mr-8'>Verify your phone</h1>
-          <p className='text-slate-500 mt-2 text-base'>We sent you an SMS code to your phone number</p>
+    <div className='flex sm:justify-center items-center px-4 sm:px-0'>
+      <div className="bg-white flex flex-col p-5 md:p-6  border-2 border-palladium rounded-xl w-full sm:max-w-[440px]">
+        <div className="flex justify-between">
+          <div>
+            <h1 className='font-medium text-[22px] leading-[130%] md:mr-8'>Verify your phone</h1>
+            <p className='text-slate-500 mt-2 text-base'>We sent you an SMS code to your phone number</p>
+          </div>
         </div>
-      </div>
-      <div className='flex gap-x-4 mt-6 md:mt-8 pb-4'>
-        {
-          code.map((value, index) => {
-            return (
-              <Input
-                key={index}
-                index={index}
-                getValue={(value, index) => {
-                  code[index] = value;
-                }}
-              />
-            )
-          })
-        }
-      </div>
-      <div className="flex mt-4 gap-x-4">
-        <button
-          onClick={() => _cancelConfirmationCode()}
-          className='rounded-xl flex gap-x-4 mb-8 text-black h-11 w-1/2 items-center justify-center px-6 border border-gray-500'>
-          Cancel
-        </button>
-        <button
-          onClick={handleClick}
-          className="bg-black rounded-xl flex h-11 w-1/2 items-center justify-center px-6">
-          <span
-            className="text-base font-light text-white">
-            Submit
-          </span>
-        </button>
+        <div className='flex gap-x-4 mt-6 md:mt-8 pb-4'>
+          {
+            code.map((value, index) => {
+              return (
+                <Input
+                  key={index}
+                  index={index}
+                  getValue={(value, index) => {
+                    code[index] = value;
+                  }}
+                />
+              )
+            })
+          }
+        </div>
+        <div className="flex mt-4 gap-x-4">
+          <button
+            onClick={() => _cancelConfirmationCode()}
+            className='rounded-xl flex gap-x-4 mb-8 text-black h-11 w-1/2 items-center justify-center px-6 border border-gray-500'>
+            Cancel
+          </button>
+          <button
+            onClick={handleClick}
+            className="bg-black rounded-xl flex h-11 w-1/2 items-center justify-center px-6">
+            <span
+              className="text-base font-light text-white">
+              Submit
+            </span>
+          </button>
+        </div>
       </div>
     </div>
   )
