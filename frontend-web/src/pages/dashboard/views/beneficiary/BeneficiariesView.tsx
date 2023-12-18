726c2138a15970553c96d59af63f594ce4fc40f0
Malik-Haziq <malikhaziq153@gmail.com>
2023-12-18 23:57:11 +0500

    update phone number requirement

--- frontend-web/src/pages/dashboard/views/beneficiary/BeneficiariesView.tsx
+++ frontend-web/src/pages/dashboard/views/beneficiary/BeneficiariesView.tsx
@@ -145,18 +145,13 @@ export default function BeneficiariesView() {
       (modalControl.backup_email2 && !isValidEmail(modalControl.backup_email2))
     ) {
       toast("please enter a valid Email address", "error")
-    } else if (
-      (!isValidPhoneWithRegion(modalControl.phone_number) &&
-        !isValidPhoneWithRegion(modalControl.backup_phone_number)) ||
-      (modalControl.phone_number &&
-        !isValidPhoneWithRegion(modalControl.phone_number)) ||
-      (modalControl.backup_phone_number &&
-        !isValidPhoneWithRegion(modalControl.backup_phone_number))
-    ) {
-      toast("please enter a valid Phone number", "error")
-    } else {
-      modalHistoryPush("Step-1")
-      setModalVisibility("Step-2")
+    } else if (modalControl.phone_number !== '' && !isValidPhoneWithRegion(modalControl.phone_number)) {
+      toast("Please enter a valid phone number", "error");
+    } else if (modalControl.backup_phone_number !== '' && !isValidPhoneWithRegion(modalControl.backup_phone_number)) {
+      toast("Please enter a valid phone number", "error");
+    } else{
+        modalHistoryPush("Step-1")
+        setModalVisibility("Step-2")
     }
   }
   const _submitStepTwoModal = () => {
@@ -165,7 +160,7 @@ export default function BeneficiariesView() {
       !modalControl.instagram_username &&
       !modalControl.twitter_username
     ) {
-      toast("Atleast 1 social media accounts is compulsory", "error")
+      toast("At least 1 social media accounts is compulsory", "error")
     } else {
       modalHistoryPush("Step-2")
       setModalVisibility("Step-3")
