2e77c16e9375ed1eb0ef37ef1db75b3b54d04da8
Malik-Haziq <malikhaziq153@gmail.com>
2024-01-31 22:07:08 +0500

    update

--- frontend-web/src/components/Pagination/index.tsx
+++ frontend-web/src/components/Pagination/index.tsx
@@ -45,6 +45,7 @@ export function Pagination(_props: {
   return (
     <div className="flex items-center justify-between p-5">
       <button
+        data-cy="show-previous-user-page-button"
         onClick={_previousPage}
         disabled={_props.currentPage == 1}
         className={
@@ -74,6 +75,7 @@ export function Pagination(_props: {
         ))}
       </div>
       <button
+        data-cy="show-next-user-page-button"
         onClick={_nextPage}
         disabled={_props.currentPage == _props.totalPages}
         className={
